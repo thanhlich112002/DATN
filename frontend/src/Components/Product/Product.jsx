4e350b922e98a192ee7d526432be53d4f6625f65
LAPTOP-M0F2IN9G\this PC <nguyenthanhlich112002@gmail.com>
2024-06-27 01:21:23 +0700

    12

--- frontend/src/Components/Product/Product.jsx
+++ frontend/src/Components/Product/Product.jsx
@@ -125,6 +125,7 @@ function Product({ setIsLoading }) {
     fetchIsComents();
     try {
       await createComment(formData, product._id);
+      setIsComment(false);
       window.location.reload();
     } catch (error) {
       console.log(error.response.data.message);
